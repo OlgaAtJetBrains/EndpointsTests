6e8b2ca4cfca4875393334eb19c3575c3027909c
olga.varfolomeeva <olga.varfolomeeva@jetbrains.com>
2021-07-12 18:52:59 +0300

    second prj

---
+++ OtherEndpointsTests/AnotherMapCustomConstraint.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace OtherEndpointsTests
+{
+    public class AnotherMapCustomConstraint
+    {
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (!values.TryGetValue(routeKey, out var routeValue)) return false;
+
+            var parameterValueString = Convert.ToString(routeValue, CultureInfo.InvariantCulture);
+            return IsEmailAddressValid(parameterValueString);
+        }
+        private bool IsEmailAddressValid(string emailAddress)
+        {
+            return true;
+        }
+    }
+}
---
+++ OtherEndpointsTests/AspNet/AnotherMapCustomConstraintController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OtherEndpointsTests.AspNet
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnotherMapCustomConstraintController  : ControllerBase
+    {
+        [HttpGet("/{test:AnotherMapCustomConstraint}")]
+        public void ActionMethod(AnotherMapCustomConstraint test) { }
+
+    }
+}
