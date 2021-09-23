2d1937db37e8e8a0a8a396f6d20e16dc5a511f0b
olga.varfolomeeva <olga.varfolomeeva@jetbrains.com>
2021-09-23 17:02:22 +0300

    Renamed Controller

--- EndpointsTests/AspNet/SlashController.cs
+++ EndpointsTests/AspNet/SlashController.cs
@@ -8,13 +8,18 @@ namespace EndpointsTests.AspNet
     {
         // If the route starts with "/" or "~/" then the route
         // should be displayed without Controller's route in endpoints viewer
+     
+        private const string Slash = "~/";
         
         // should be /valid
         [HttpPut("/valid")]
         // should be /valid
         [HttpPut("~/valid")]
+        // should be /valid
+        // due to the slash in first string value
+        [HttpGet(Slash + "valid")]
         // should be /api/Slash/valid
-        [HttpPut("valid")]
+        [HttpGet("valid")]
         public void Action1() {}
     }
 }
---
+++ EndpointsTests/AspNet/SplitsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EndpointsTests.AspNet
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Area("Area")]
+    public class SplitsController : ControllerBase
+    {
+        private const string ConstValue = "const";
+        private const string item1 = "item";
+        private const string item2 = "/";
+        
+        // Should be displayed as `api/Gaps/{const}`
+        [HttpPut("valid/{" + ConstValue + "}")]
+        // Should not be displayed in Endpoints tab
+        [HttpPut(template: "invalid/{" + test +"}")]
+        // Should show `Parameter name expected` highlighting 
+        [HttpPut(template: "invalid/{" + null +"}")]
+        // Should show `Parameter name expected` highlighting 
+        // Should also show that class name is not expected here 
+        [HttpPut(template: "invalid/{" + Program +"}")]
+        // Type "" inside any token
+        // Should not raise RSRP-486121
+        [HttpGet("[controller]/[action]/[area]")]
+        // Should show highlighting that catch all section is misplaced
+        // see RSRP-486137, check after 221
+        [HttpGet("test{*" + ConstValue +"}")]
+        
+        
+        public void Action1() {}
+        
+
+    }
+}
