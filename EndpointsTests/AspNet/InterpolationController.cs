d75aff9f71f0ecf97262b539338f69f9de0b3245
olga.varfolomeeva <olga.varfolomeeva@jetbrains.com>
2021-10-27 18:45:17 +0300

    Added InterpolationController

---
+++ EndpointsTests/AspNet/InterpolationController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndpointsTests.AspNet;
+
+[Route("api/[controller]")]
+[ApiController]
+public class InterpolationController : ControllerBase
+{
+    private const string SomeVar = "test";
+
+    // Should not raise any issues when route contains string interpolation
+
+    // The route in endpoints should contain interpolated part
+
+    [HttpGet($"{SomeVar}" + "{someVar2}")]
+    public void Action(string someVar2) { }
+
+}
